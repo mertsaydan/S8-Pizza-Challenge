4a1a6f493762c28f4e200572868163504589591c
mertsaydan <mertsaydan85@gmail.com>
2026-06-09 21:56:08 +0300

    Icons moved to public

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,27 +4,27 @@ export default function Nav() {
     <section>
       <nav className="menu-bar">
         <div className="menu-items">
-          <img src="src/assets/icons/1.svg" />
+          <img src="/icons/1.svg" />
           <p>YENİ! Kore</p>
         </div>
         <div className="menu-items">
-          <img src="src/assets/icons/2.svg" />
+          <img src="/icons/2.svg" />
           <p>Pizza</p>
         </div>
         <div className="menu-items">
-          <img src="src/assets/icons/3.svg" />
+          <img src="/icons/3.svg" />
           <p>Burger</p>
         </div>
         <div className="menu-items">
-          <img src="src/assets/icons/4.svg" />
+          <img src="/icons/4.svg" />
           <p>Kızartmalar</p>
         </div>
         <div className="menu-items">
-          <img src="src/assets/icons/5.svg" />
+          <img src="/icons/5.svg" />
           <p>Fast Food</p>
         </div>
         <div className="menu-items">
-          <img src="src/assets/icons/6.svg" />
+          <img src="/icons/6.svg" />
           <p>Gazlı İçecek</p>
         </div>
       </nav>
