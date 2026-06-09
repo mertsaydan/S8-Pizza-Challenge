f7a9a40b64b37fd49ad77a64fe53f61f72b91633
mertsaydan <mertsaydan85@gmail.com>
2026-06-09 21:40:55 +0300

    Bug fixes

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 import Nav from './Nav.jsx';
 import Suggest from './Suggest.jsx';
 import Products from './Products.jsx';
-import Header from './Header.jsx';
+import Header from './Header';
 
 export default function Intro() {
   const history = useHistory();
